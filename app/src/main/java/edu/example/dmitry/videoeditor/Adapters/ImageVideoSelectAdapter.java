50d6bfa5784dada8aeab417a46822ed2e5b6c014
dmitry <dvpitakov@gmail.com>
2017-11-14 00:06:21 +0300

    Доработана многопоточность

--- app/src/main/java/edu/example/dmitry/videoeditor/Adapters/ImageVideoSelectAdapter.java
+++ app/src/main/java/edu/example/dmitry/videoeditor/Adapters/ImageVideoSelectAdapter.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -36,26 +38,35 @@ import edu.example.dmitry.videoeditor.SettingsVideo;
 public class ImageVideoSelectAdapter  extends ArrayAdapter<String> {
     List<String> strings;
     Activity activity;
+
+    private ExecutorService service = Executors.newFixedThreadPool(2);
+    private static Handler handler = new MyHandler();
+    private static class MyHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            ViewAndString viewAndString = (ViewAndString)msg.obj;
+            ((ImageView)viewAndString.view.findViewById(R.id.image_pos)).setImageBitmap(viewAndString.bm);
+        }
+    }
+
     public ImageVideoSelectAdapter(@NonNull Activity activity, int resource, @NonNull List<String> objects) {
         super(activity, resource, objects.subList(0, objects.size()));
         strings = objects;
         this.activity = activity;
     }
 
-    MyTask myTask = null;
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         if (true) {
-            if (myTask == null) {
-                myTask = new MyTask();
-            }
+
             convertView = LayoutInflater.from(getContext())
                     .inflate(R.layout.photo_video_tip_rect
                             , parent
                             , false);
             convertView.setTag(strings.get(strings.size() - 1 - position));
-            new MyTask().execute(new ViewAndString(convertView, strings.get(strings.size() - 1 - position), null));
+            service.submit(new MyRunnable(new ViewAndString(convertView, strings.get(strings.size() - 1 - position), null)));
 
 
         }
@@ -73,29 +84,27 @@ public class ImageVideoSelectAdapter  extends ArrayAdapter<String> {
         public String string;
         public Bitmap bm;
     }
-    private class MyTask extends AsyncTask<ViewAndString, ViewAndString, ViewAndString> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+    private class MyRunnable implements Runnable {
+
+        public MyRunnable(ViewAndString viewAndString) {
+            setViewAndString(viewAndString);
         }
 
-        @Override
-        protected void onProgressUpdate(ViewAndString... result) {
-            super.onProgressUpdate(result);
-            ((ImageView)result[0].view.findViewById(R.id.image_pos)).setImageBitmap(result[0].bm);
+        public void setViewAndString(ViewAndString viewAndString) {
+            this.viewAndString = viewAndString;
         }
-
+        private ViewAndString viewAndString = null;
+        private Bitmap result = null;
         @Override
-        protected ViewAndString doInBackground(ViewAndString... params) {
-            Bitmap bm = null;
+        public void run() {
             try {
-                File imageFile = new File(params[0].string);
+                File imageFile = new File(viewAndString.string);
                 BitmapFactory.Options options = new BitmapFactory.Options();
                 options.inSampleSize = 8;
                 if (imageFile.exists()) {
                     try {
-                        bm = BitmapFactory.decodeFile(params[0].string, options);
+                        result = BitmapFactory.decodeFile(viewAndString.string, options);
                     } catch (OutOfMemoryError oushit) {
                         Log.d("err", oushit.toString());
                     }
@@ -103,45 +112,12 @@ public class ImageVideoSelectAdapter  extends ArrayAdapter<String> {
                     throw new IOException();
                 }
             } catch(IOException e){
-                bm = null;
+                result = null;
             }
-            params[0].bm = bm;
-            return params[0];
-
+            viewAndString.bm = result;
+            Message msg = handler.obtainMessage(0, viewAndString);
+            handler.sendMessage(msg);
         }
-
-        @Override
-        protected void onPostExecute(ViewAndString result) {
-            super.onPostExecute(result);
-            ((ImageView)result.view.findViewById(R.id.image_pos)).setImageBitmap(result.bm);
-        }
-/*
-        ViewAndString viewAndString = null;
-        public void go() {
-            ExecutorService service = Executors.newFixedThreadPool(2);
-            service.submit(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        File imageFile = new File(params[0].string);
-                        BitmapFactory.Options options = new BitmapFactory.Options();
-                        options.inSampleSize = 8;
-                        if (imageFile.exists()) {
-                            try {
-                                bm = BitmapFactory.decodeFile(params[0].string, options);
-                            } catch (OutOfMemoryError oushit) {
-                                Log.d("err", oushit.toString());
-                            }
-                        } else {
-                            throw new IOException();
-                        }
-                    } catch(IOException e){
-                        bm = null;
-                    }
-                    params[0].bm = bm;
-                }
-            })
-        }*/
     }
 
 }
